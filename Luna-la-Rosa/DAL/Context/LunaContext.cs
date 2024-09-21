0460ccf604f2c1adfc1c022100397af8b13e212f
surikadt <gnidanol@gmail.com>
2024-09-21 13:08:47 +0300

    Implemented DbContext

---
+++ Luna-la-Rosa/DAL/Context/LunaContext.cs
@@ -0,0 +1,49 @@
+using DAL.Context.Configuration;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Context;
+
+public class LunaContext : DbContext
+{
+    public LunaContext(DbContextOptions<LunaContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Flower> Flowers { get; set; }
+    public DbSet<Bouquet> Bouquets { get; set; }
+    public DbSet<BouquetCategory> BouquetCategories { get; set; }
+    public DbSet<BouquetCategoryAssociation> BouquetCategoryAssociations { get; set; }
+    public DbSet<BouquetFlower> BouquetFlowers { get; set; }
+    public DbSet<CustomBouquet> CustomBouquets { get; set; }
+    public DbSet<CustomBouquetFlower> CustomBouquetFlowers { get; set; }
+    public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderBouquet> OrderBouquets { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<AddOn> AddOns { get; set; }
+    public DbSet<OrderAddOn> OrderAddOns { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new FlowerConfiguration());
+        modelBuilder.ApplyConfiguration(new BouquetConfiguration());
+        modelBuilder.ApplyConfiguration(new BouquetCategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new BouquetCategoryAssociationConfiguration());
+        modelBuilder.ApplyConfiguration(new BouquetFlowerConfiguration());
+        modelBuilder.ApplyConfiguration(new CustomBouquetConfiguration());
+        modelBuilder.ApplyConfiguration(new CustomBouquetFlowerConfiguration());
+        modelBuilder.ApplyConfiguration(new ShoppingCartConfiguration());
+        modelBuilder.ApplyConfiguration(new CartItemConfiguration());
+        modelBuilder.ApplyConfiguration(new OrderConfiguration());
+        modelBuilder.ApplyConfiguration(new OrderBouquetConfiguration());
+        modelBuilder.ApplyConfiguration(new PaymentConfiguration());
+        modelBuilder.ApplyConfiguration(new AddOnConfiguration());
+        modelBuilder.ApplyConfiguration(new OrderAddOnConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
