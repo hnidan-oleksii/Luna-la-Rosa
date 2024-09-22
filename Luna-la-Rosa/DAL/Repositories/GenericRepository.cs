cc842a6dbbf5724611b32af039080268df5ff4dd
user <katya.rotar02@gmail.com>
2024-09-23 00:22:22 +0300

    Implemented Generic Repository pattern

---
+++ Luna-la-Rosa/DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using DAL.Context;
+using DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories;
+
+public class GenericRepository<T>(LunaContext context) : IGenericRepository<T>
+    where T : class
+{
+    public async Task<T> GenerateGetByIdAsync(int id)
+    {
+        var entity = await context.Set<T>().FindAsync(id);
+    
+        if (entity == null)
+        {
+            throw new KeyNotFoundException($"Entity with ID {id} not found.");
+        }
+    
+        return entity;
+    }
+
+    public async Task<IEnumerable<T>> GenerateGetAllAsync()
+    {
+        return await context.Set<T>().ToListAsync();
+    }
+
+    public async Task GenerateAddAsync(T entity)
+    {
+        await context.Set<T>().AddAsync(entity);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task GenerateUpdateAsync(T entity)
+    {
+        context.Set<T>().Update(entity);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task GenerateDeleteAsync(T entity)
+    {
+        context.Set<T>().Remove(entity);
+        await context.SaveChangesAsync();
+    }
+}
---
+++ Luna-la-Rosa/DAL/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace DAL.Repositories.Interfaces;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GenerateGetAllAsync();
+    Task GenerateAddAsync(T entity);
+    Task GenerateUpdateAsync(T entity);
+    Task GenerateDeleteAsync(T entity);
+}
