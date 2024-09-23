c83acf78aa1162529ff095bc883ce1b0fa82251a
Katya-Rotar <125387399+katya-rotar@users.noreply.github.com>
2024-09-23 10:33:33 +0300

    Added implementation of Unit of Work pattern

---
+++ Luna-la-Rosa/DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Context;
+using DAL.Repositories.Interfaces;
+
+namespace DAL.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly LunaContext _context;
+
+    public UnitOfWork(LunaContext context)
+    {
+        _context = context;
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+
+    public async Task SaveAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
