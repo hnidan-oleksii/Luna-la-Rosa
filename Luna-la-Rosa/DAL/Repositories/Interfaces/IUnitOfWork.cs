db4e69553b8a6527fdcb6bcf30ff72f62eb973ee
Katya-Rotar <125387399+Katya-Rotar@users.noreply.github.com>
2024-09-24 09:22:11 +0300

    Added implementation of Unit of Work pattern (#2)
    * Added implementation of Unit of Work pattern
    
    * Added transaction handling to UnitOfWork: Begin, Commit, Rollback

---
+++ Luna-la-Rosa/DAL/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace DAL.Repositories.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    Task SaveAsync();
+    Task BeginTransactionAsync(CancellationToken cancellationToken);
+    Task CommitTransactionAsync(CancellationToken cancellationToken);
+    Task RollbackTransactionAsync(CancellationToken cancellationToken);
+}
