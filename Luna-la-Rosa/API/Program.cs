5dab6904bfa954bc8ccc04b3f44d2f4dbfdfa9bb
Katya-Rotar <125387399+katya-rotar@users.noreply.github.com>
2024-10-04 22:02:41 +0300

    Created AddOnsController and registered services in Program.cs

--- Luna-la-Rosa/API/Program.cs
+++ Luna-la-Rosa/API/Program.cs
@@ -1,8 +1,32 @@
+using BLL;
+using BLL.DTO;
+using BLL.Services;
+using BLL.Services.Interfaces;
+using BLL.Validation;
+using DAL.Repositories;
+using DAL.Repositories.Interfaces;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+//Fluent validation
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
+builder.Services.AddScoped<IValidator<AddOnDto>, AddOnDtoValidator>();
+
+builder.Services.AddScoped<IAddOnRepository, AddOnRepository>();
+
+builder.Services.AddScoped<IAddOnService, AddOnService>();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
