00531cba161a9f585b9e9f4741044386104a4977
Katya-Rotar <125387399+katya-rotar@users.noreply.github.com>
2024-10-04 19:10:50 +0300

    Added AddOnRepository with method to group add-ons by type and updated GenericRepository for context handling improvements

---
+++ Luna-la-Rosa/DAL/Repositories/AddOnRepository.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Context;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+
+namespace DAL.Repositories;
+
+public class AddOnRepository : GenericRepository<AddOn>, IAddOnRepository
+{
+    public AddOnRepository(LunaContext context) : base(context)
+    {
+    }
+
+    public async Task<Dictionary<string, List<AddOn>>> GetAddOnsGroupedByTypeAsync()
+    {
+        var addOns = await GetAllAsync();
+        var groupedAddOns = addOns
+            .GroupBy(addOn => addOn.Type.Name)
+            .ToDictionary(k => k.Key, g => g.ToList());
+
+        return groupedAddOns;
+    }
+}
