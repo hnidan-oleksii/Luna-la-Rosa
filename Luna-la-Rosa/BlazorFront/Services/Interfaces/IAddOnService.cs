ea828881574d0a1b4f93393d2092c1096f5bace9
Oleg <oleg.rusnak04@gmail.com>
2024-11-28 15:53:22 +0200

    changes in pages

---
+++ Luna-la-Rosa/BlazorFront/Services/Interfaces/IAddOnService.cs
@@ -0,0 +1,21 @@
+﻿
+using BLL.DTO.AddOn;
+using DAL.Helpers.Params;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorFront.Services.Interfaces
+{
+
+    public interface IAddOnService
+    {
+        Task<IEnumerable<AddOnDto>> GetAllAddOnsAsync(string searchQuery);
+        Task<Dictionary<string, List<AddOnDto>>> GetAddOnsGroupedByTypeAsync();
+        Task<AddOnDto> GetAddOnByIdAsync(int id);
+        Task<int> AddAddOnAsync(CreateAddOnDto addOnDto, CancellationToken cancellationToken);
+        Task UpdateAddOnAsync(AddOnDto addOnDto, CancellationToken cancellationToken);
+        Task DeleteAddOnAsync(int id, CancellationToken cancellationToken);
+    }
+
+}
