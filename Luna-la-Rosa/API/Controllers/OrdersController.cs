2a59fa79e86dec897950024d8c3155fc6190568b
surikadt <gnidanol@gmail.com>
2024-11-28 22:10:39 +0200

    add orders controller

---
+++ Luna-la-Rosa/API/Controllers/OrdersController.cs
@@ -0,0 +1,67 @@
+using BLL.DTO.Order;
+using BLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController : ControllerBase
+{
+    private readonly IOrderService _orderService;
+
+    public OrdersController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<OrderDto>>> GetAllOrders()
+    {
+        var orders = await _orderService.GetAllOrdersAsync();
+        return Ok(orders);
+    }
+
+    [HttpGet("users/{userId:int}")]
+    public async Task<ActionResult<IEnumerable<OrderDto>>> GetOrdersByUserId(int userId)
+    {
+        var orders = await _orderService.GetAllOrdersByUserIdAsync(userId);
+        return Ok(orders);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<OrderDto>> GetOrder(int id)
+    {
+        var order = await _orderService.GetOrderByIdAsync(id);
+        return Ok(order);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder([FromBody] CreateOrderDto createOrderDto,
+        CancellationToken cancellationToken)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        await _orderService.OrderItemsFromCartAsync(createOrderDto, cancellationToken);
+        return Created();
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> UpdateOrder(int id, [FromBody] OrderDto orderDto,
+        CancellationToken cancellationToken)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+
+        if (id != orderDto.Id) return BadRequest("ID mismatch");
+
+        await _orderService.UpdateOrderAsync(orderDto, cancellationToken);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteOrder(int id, CancellationToken cancellationToken)
+    {
+        await _orderService.DeleteOrderAsync(id, cancellationToken);
+        return NoContent();
+    }
+}
