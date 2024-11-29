41d1c6d4fcef392baa5181583d182cbe3f0b5f9f
surikadt <gnidanol@gmail.com>
2024-11-29 22:09:58 +0200

    add property in ItemAddOnDto for simplicity
    absence of CustomBouquetId field in ItemsAddOnDto caused mapping issues

--- Luna-la-Rosa/BLL/DTO/ItemAddOn/ItemAddOnDto.cs
+++ Luna-la-Rosa/BLL/DTO/ItemAddOn/ItemAddOnDto.cs
@@ -5,7 +5,8 @@ namespace BLL.DTO.ItemAddOn;
 public class ItemAddOnDto
 {
     public int Id { get; set; }
-    public int BouquetId { get; set; }
+    public int? BouquetId { get; set; }
+    public int? CustomBouquetId { get; set; }
     public int AddOnId { get; set; }
     public int Quantity { get; set; }
     public string? CardNote { get; set; }
