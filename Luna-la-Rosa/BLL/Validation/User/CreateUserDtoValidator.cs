067d1835386a5ca9a30a44944c291ba405dd6ee5
Katya-Rotar <katya.rotar02@gmail.com>
2024-12-01 15:13:44 +0200

    Edited User Addition Method and Updated Validator

---
+++ Luna-la-Rosa/BLL/Validation/User/CreateUserDtoValidator.cs
@@ -0,0 +1,30 @@
+﻿using BLL.DTO.User;
+using FluentValidation;
+
+namespace BLL.Validation.User
+{
+    public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+    {
+        public CreateUserDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(x => x.PasswordHash)
+                .NotEmpty().WithMessage("Password is required.");
+
+            RuleFor(x => x.FirstName) 
+                .Matches(@"^[a-zA-Z]+$").WithMessage("Invalid first name format.") 
+                .When(x => !string.IsNullOrEmpty(x.FirstName)); 
+            
+            RuleFor(x => x.LastName) 
+                .Matches(@"^[a-zA-Z]+$").WithMessage("Invalid last name format.") 
+                .When(x => !string.IsNullOrEmpty(x.LastName));
+
+            RuleFor(x => x.PhoneNumber)
+                .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Invalid phone number format.")
+                .When(x => !string.IsNullOrEmpty(x.PhoneNumber));
+        }
+    }
+}
